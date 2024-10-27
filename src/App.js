3ba5999c6fdfd9ef7cc739fa1662b632069e8f93
JILDERGANO1 <jilchurchdergano.1@gmail.com>
2024-10-27 19:41:03 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+      <h1>Ciao, mi chiamo Joel!</h1>
+      </div>
+      <div>
+        <p>Sono di Messina e mi piacciono i cannoli!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
